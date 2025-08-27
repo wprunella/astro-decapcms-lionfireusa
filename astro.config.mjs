5a066ca0c6e2b4e38f6bf2c8fbdf48528aea0c24
wprunella <43684994+wprunella@users.noreply.github.com>
2025-08-27 15:25:26 -0400

    Update astro.config.mjs add image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import image from 'astro/image';
 
 export default defineConfig({
   integrations: [
     tailwind(),
     mdx(),
+    image(),
   ],
 });
