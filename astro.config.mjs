61104f0b2cb278bfc7889aa788dbff3e2b15ceea
wprunella <43684994+wprunella@users.noreply.github.com>
2025-08-27 15:51:37 -0400

    Remove deprecated @astrojs/image integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
-import image from '@astrojs/image';
 
 export default defineConfig({
   integrations: [
     tailwind(),
     mdx(),
-    image(),
   ],
 });
